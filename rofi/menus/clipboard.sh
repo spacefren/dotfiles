43cc2d691e0c92c6fdb6a8ba438ca5602f88c47d
spacefren <116370539+spacefren@users.noreply.github.com>
2025-11-10 19:22:32 +0100

    Better clipboard management

--- rofi/menus/clipboard.sh
+++ rofi/menus/clipboard.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-selected=$(clipvault list | awk -F$'\t' '{print $2 "\t" $1}' | rofi -dmenu -i -sync -p "Clipboard")
-id=$(printf '%s' "$selected" | awk -F$'\t' '{print $NF}')
-[ -n "$id" ] && clipvault get --index "$id" | wl-copy && ydotool key 29:1 47:1 47:0 29:0
+# Don't judge, I'm new :sob:
+selected=$(clipvault list | awk -F "\t" '{print $2 "\t\t\t\t\t\t\t\t\t" $1}' | rofi -dmenu -i -sync -p "Clipboard" | awk -F "\t" '{print $10}')
+echo "$selected"
+[ -n "$selected" ] && clipvault get "$selected" | wl-copy | ydotool key 29:1 47:1 47:0 29:0
